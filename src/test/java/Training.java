ea0f4ecd5594b01cfb68c41bf7b6408491279262
Maria_Nour <marinour@mail.ru>
2023-03-06 23:43:07 +0300

    added examples

---
+++ src/test/java/Training.java
@@ -0,0 +1,12 @@
+public class Training {
+    public static void main(String[] args) {
+        //Arithmetic operators
+        int a = 1;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("Addition result: " + sum);
+        int sub = a - b;
+        System.out.println("Subtraction result: " + sub);
+
+         }
+}
